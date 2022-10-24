8ec6cfe9dc805db199ef100a96e79f3b47192e17
Dennis Dollée <dennisdollee@gmail.com>
2022-10-24 18:52:36 +0200

    Add DE and (glimma) plots

--- DESeqNormalization.R
+++ DESeqNormalization.R
@@ -34,6 +34,8 @@ counts_clin <- counts_clin[intersect, ]
 all(ncol(counts[, intersect]) == nrow(counts_clin[intersect, ]))
 all(nrow(counts_clin[intersect, ]) == ncol(counts[, intersect]))
 
+factor(dds$Responder_type)
+
 ## Create DESeq dataset
 dds <- DESeqDataSetFromMatrix(
   countData = counts,
@@ -41,22 +43,70 @@ dds <- DESeqDataSetFromMatrix(
   design = ~Responder_type
 )
 
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep, ]
+
 ## Add responder types as factors
 dds$Responder_type <- factor(dds$Responder_type, levels = c("0", "1", "2"))
 
-dds <- DESeq(dds)
+dds$Responder_type <- relevel(dds$Responder_type, ref = "0")
+
+dds$Responder_type <- droplevels(dds$Responder_type)
 
 ## Run differential expression
 dds <- DESeq(dds)
 res <- results(dds)
 res
 
+results(dds, name = "Responder_type_2_vs_0")
+results(dds, name = "Responder_type_1_vs_0")
+
 resultsNames(dds)
 
+## Shrinking Log Fold Change
+resultsNames(dds)
+reslfc <- lfcShrink(dds,
+  contrast = c("Responder_type", "1", "2"),
+  type = "ashr"
+)
+reslfc
+
+## Order results by smalles p value
+res_ordered <- res[order(res$pvalue), ]
+
+## Results summary
 summary(res)
 
+## Results summary with p value < 0.1
+sum(res$padj < 0.1, na.rm = TRUE)
+
+## Results summary with alpha of 0.05
 res05 <- results(dds, alpha = 0.05)
 summary(res05)
+sum(res05$padj < 0.05, na.rm = TRUE)
+
+library(Glimma)
+
+glimmaMDS(dds)
+
+glimmaMA(dds)
+
+## MA plot
+plotMA(res, ylim = c(-2, 2))
+
+## MA plot with shrunken LFC
+plotMA(resLFC, ylim = c(-2, 2))
+
+## Read counts for the gene with the lowest p value
+plotCounts(dds, gene = which.min(res$padj), intgroup = "Responder_type")
+
+d <- plotCounts(dds, gene = which.min(res$padj), intgroup = "Responder_type", returnData = TRUE)
+library("ggplot2")
+ggplot(d, aes(x = Responder_type, y = count)) +
+  geom_point(position = position_jitter(w = 0.1, h = 0)) +
+  scale_y_log10(breaks = c(25, 100, 400))
+
+mcols(res)$description
 
 sum(res$padj < 0.1, na.rm = TRUE)
 sum(res05$padj < 0.05, na.rm = TRUE)
